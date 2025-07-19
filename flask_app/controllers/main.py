8d052af22605353bcfb14f209f3dcfe60c7db4a8
Tom Peltier <tommypeltier@gmail.com>
2025-07-19 00:52:37 -0400

    match data retrieval functional

--- flask_app/controllers/main.py
+++ flask_app/controllers/main.py
@@ -1,13 +1,31 @@
 from flask_app import app
-from flask import render_template,redirect,session,request,flash
+from flask import render_template,redirect,session,request,flash,request
 from flask_app.models import players
 from flask_bcrypt import Bcrypt
 import logging
-import json
+import json, os, requests
 import datetime
+import dotenv
+from os.path import dirname
 bcrypt = Bcrypt(app)
+
+PROD_API_URL = "https://open.faceit.com/data/v4"
+
+@app.route('/api/stats', methods=['POST'])
+def get_stats():
+    headers = {
+        "Authorization" : "Bearer " + os.environ.get("API_KEY")
+    }
+    match_id = request.form['ID']
+    url = PROD_API_URL + f'/matches/{match_id}/stats'
+    response = requests.get(url=url, headers=headers)
+    print(response.json())
+    with open('output.json', "w") as file:
+        json.dump(response.json(), file, indent=4)
+    return redirect('/')
 
 
 @app.route('/')
 def home():
+    
     return render_template("home.html")
